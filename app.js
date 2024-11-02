4df36191f45640a90a2c1c97df40f2a83a683f90
abd el mouiz saadani <abdoazzi001@gmail.com>
2024-11-02 20:05:39 +0100

    implementing sidebar it's style and js logic

---
+++ app.js
@@ -0,0 +1,22 @@
+const toggleButton = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
+
+function toggleSidbar() {
+  sidebar.classList.toggle("close");
+  toggleButton.classList.toggle("rotate");
+
+  Array.from(sidebar.getElementsByClassName("show")).forEach((ul) => {
+    ul.classList.remove("show");
+    ul.previousElementSibling.classList.remove("rotate");
+  });
+}
+
+function toggleSubMenu(button) {
+  button.nextElementSibling.classList.toggle("show");
+  button.classList.toggle("rotate");
+
+  if (sidebar.classList.contains("close")) {
+    sidebar.classList.toggle("close");
+    toggleButton.classList.toggle("rotate");
+  }
+}
