5027480c5a77fb6c72df6afbbd58893858b084bb
pharaohSamma <mohyisaadani001@gmail.com>
2024-11-04 23:26:36 +0100

    showing data in the view

--- src/controller.js
+++ src/controller.js
@@ -2,25 +2,31 @@ import { Model } from "./model.js";
 
 
 const modelInstance = new Model;
-const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTQ5YTBkZjcxNWI5MGVlN2VjOTI3YjhjOWMwOGI2MiIsIm5iZiI6MTczMDczNDEwNy4wMTY1MDY0LCJzdWIiOiI2NzFlYmY0OTViZTllODc1OWRhN2E3NzgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2TOdzGsRpCcZn-Jpc64TkM0QFSQpXKXXLcMnumLd6CY'; 
+// Base URL for images
+const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMTQ5YTBkZjcxNWI5MGVlN2VjOTI3YjhjOWMwOGI2MiIsIm5iZiI6MTczMDczNDEwNy4wMTY1MDY0LCJzdWIiOiI2NzFlYmY0OTViZTllODc1OWRhN2E3NzgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2TOdzGsRpCcZn-Jpc64TkM0QFSQpXKXXLcMnumLd6CY'; 
 
 
 const getMovies = async function () {
     try {
-        const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', {
+        const response = await fetch('https://api.themoviedb.org/3/discover/movie', {
             method: 'GET',
             headers:
            {
-              'Authorization': `Bearer ${apiKey}`,
+              'Authorization': `Bearer ${accessToken}`,
               'accept': 'application/json'
             }
           });
-          console.log(response);
+          
           if(!response.ok){
             throw new Error(`Error retreiving movie data ${response.status}`)
           }
           const data = await response.json();
-          modelInstance.movieList =  data;
+          console.log(data);
+          data.results.forEach(movie => {
+            movie.poster_path = imageBaseUrl+movie.poster_path;
+          });
+          return data;
     } catch (error) {
         throw new Error(`error: ${error.message}`)
     }
--- src/view.js
+++ src/view.js
@@ -6,12 +6,19 @@ const modelInstance = new Model;
 const movieView = document.getElementById('movieList');
 movieView.innerHTML = '';
 
-
+window.addEventListener('load', async()=>{
+    moviesView();
+})
 async function moviesView(){
     const moviesList = await getMovies();
-    movieView.innerHTML=`
-    <h2>test</h2>
-    `;
-    movieView.appendChild(movieView);
+    console.log('movies list',moviesList);
+    moviesList.results.forEach(movie => {
+        movieView.innerHTML +=`
+        <div class="movieItem">
+        <img src="${movie.poster_path}">
+        <h2>${movie.original_title}</h2>
+        </div>
+        `
+    });
 }
 
